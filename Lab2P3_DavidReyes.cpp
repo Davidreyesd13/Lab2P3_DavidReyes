60179eb1cbab7482b7591b65b1698a7e0019ec65
Davidreyesd13 <davidreyesd13@gmail.com>
2023-04-28 14:14:29 -0600

    Ejercicio 2

--- Lab2P3_DavidReyes.cpp
+++ Lab2P3_DavidReyes.cpp
@@ -1,19 +1,46 @@
-// Lab2P3_DavidReyes.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
-
+using namespace std;
+int factorial(int,int);
+void tringulo(int);
 int main()
 {
-    std::cout << "Hello World!\n";
+	int opcion,fact;
+	cout << "1.Ejercicio 1\n2.Ejercicio 2\n0.Salir ";
+	cin >> opcion;
+	while (opcion!=0) {
+		switch (opcion) {
+		case 1:
+			
+			break;
+		case 2:
+			cout << "ingrese el numero que desea saber su factorial: ";
+			cin >> fact;
+			cout << "factorial:"<<factorial(fact,1)<<endl;
+			tringulo(factorial(fact, 1));
+			break;
+		}
+		cout << "1.Ejercicio 1\n2.Ejercicio 2\n0.Salir ";
+		cin >> opcion;
+   }
+	
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+int factorial(int num,int result) {
+	;
+	if (num == 0) {
+		result *= 1;
+		return result;
+	}
+	else {
+		result *= num ;
+		return factorial(num-1, result);
+	}
+}
+void tringulo(int tam) {
+	for (int i = 0; i <= tam; i++)
+	{
+		for (int j = 1; j <= i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
